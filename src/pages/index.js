382cd808a4cff7b52abeb6408273fbf3bd05e10e
cromagnonninja <bhanubhandari99@gmail.com>
2020-08-13 06:44:55 +0530

    new index.js

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+import Hero from "./index/Hero";
+// import HighlightedProducts from "./HighlightedProducts";
+
+export default () => (
+  <Layout title="Home">
+    <Hero />
+    {/* <HighlightedProducts /> */}
+  </Layout>
+);
