8936d4e73dac923672ed8667788015c80ece777a
cromagnonninja <bhanubhandari99@gmail.com>
2020-08-29 08:34:43 +0530

    debug

--- src/components/Product.js
+++ src/components/Product.js
@@ -89,9 +89,14 @@ const Person = ({ profile }) => {
 export default ({ products }) => (
     <PeopleContainer>
     <PeopleFlexContainer>
-    {products.map((p, i) => (
+    {/* {products.map((p, i) => (
         <Person key={i} profile={p} />
-      ))}
+      ))} */}
+    {console.log(products[0].fields.avatar.fields.file.url)};
+    {console.log(products[0].fields.name)};
+    {console.log(products[1].fields.name)};
+    {console.log(products[0].fields.description.content.value)};
+    {console.log(products[1].field.avatar.fields.file.url)};
     </PeopleFlexContainer>
   </PeopleContainer>
 );
